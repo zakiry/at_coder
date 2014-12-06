abf79a30319f8a27d3392862405d6a676a428b6c
Ryo Jozaki <zaki0ry@gmail.com>
2014-12-06 15:43:38 +0900

    modify b.rb

--- abc001/b.rb
+++ abc001/b.rb
@@ -6,10 +6,15 @@ vv = 0
 distance = distance.to_f/1000
 
 # 処理
-vv = distance * 10          if distance >= 0.1 && distance <= 5
-vv = distance + 50          if distance >= 6   && distance <= 30
-vv = (distance - 30)/5 + 80 if distance >= 35  && distance <= 70
-vv = 89                     if distance >  70
+if distance >= 0.1 && distance <= 5
+  vv = distance * 10
+elsif distance >= 6   && distance <= 30
+  vv = distance + 50
+elsif distance >= 35  && distance <= 70
+  vv = (distance - 30)/5 + 80
+elsif distance >  70
+  vv = 89
+end
 
 # 出力
 puts sprintf("%02d", vv.to_i)
