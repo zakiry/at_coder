7fbb7f889c8aa6c079c918a354a431be8762ee0d
Ryo Jozaki <zaki0ry@gmail.com>
2014-12-15 00:19:04 +0900

    refactoring

--- arc030/c.rb
+++ arc030/c.rb
@@ -1,55 +1,113 @@
-def get_str(nums, c)
-  str = ''
-  nums.each do |i|
-    str = str + c[i]
+@n
+@src
+@dst
+@visited
+@post_number
+@group_number
+
+def dfs1(current)
+  return if @visited[current]
+  @visited[current] = true
+  @src[current].each do |next_point|
+    dfs1 next_point
   end
-  str
+  @post_number << current
 end
 
-# 入力
-n, m, k = gets.split.map(&:to_i)
-c = gets.split(&:to_i)
-
-e = []
-n.times do
-  e << []
-end
-m.times do
-  a, b = gets.split.map(&:to_i)
-  e[b-1] << a-1
+def dfs2(current, group_id)
+  return if @group_number[current]
+  @group_number[current] = group_id
+  @dst[current].each do |next_point|
+    dfs2 next_point, group_id
+  end
 end
 
-# 初期化
-strs = []
-n.times do |i|
-  strs << {0 => [], 1 => [i]}
-end
+def dscc
+  @visited = []
+  @post_number = []
+  @n.times do |i|
+    dfs1 i if @visited[i].nil?
+  end
 
-# 処理
-1.upto(k) do |char_length|
-  n.times do |i|
-    e[i].each do |prev|
-      char_length.times do |j|
-        break if strs[prev][j].nil?
-        strs[i][j+1] = [strs[prev][j], i].flatten unless strs[prev][j].include?(i)
-        if !strs[i][j+1].nil? && !strs[prev][j+1].nil?
-          strs[i][j+1] = strs[prev][j+1] if get_str(strs[i][j+1], c) > get_str(strs[prev][j+1], c)
-        elsif !strs[prev][j+1].nil?
-          strs[i][j+1] = strs[prev][j+1]
-        end
-      end
+  @group_number = []
+  group = 0
+  @n.times do |i|
+    if @group_number[@post_number[-(i+1)]].nil?
+      dfs2 @post_number[-(i+1)], group
+      group = group + 1
     end
   end
 end
 
-# 出力
-answers = []
-strs.each do |nums|
-  answers << get_str(nums[k], c) unless nums[k].nil?
+# 入力
+@n, m, k = gets.split.map(&:to_i)
+c = gets.split(&:to_i)
+
+@src = Array.new(@n){[]}
+@dst = Array.new(@n){[]}
+m.times do
+  a, b = gets.split.map(&:to_i)
+  @src[a-1] << b-1
+  @dst[b-1] << a-1
 end
-if answers.length==0
-puts -1
+
+# 処理
+# 強連結分解
+dscc
+# グラフを再構築する
+n = @group_number.max + 1
+
+claster_str = Array.new(n){[]}
+@n.times do |i|
+  group_id = @group_number[i]
+  claster_str[group_id] << c[i]
+end
+claster_str.map! { |str_array| str_array.sort.join }
+
+strs = []
+n.times do |i|
+  str = claster_str[i]
+  hash = { 0 => '' }
+  str.length.times do |len|
+    break if len >= k
+    hash[len+1] = str[0..len]
+  end
+  strs << hash
+end
+
+src = Array.new(n){[]}
+@n.times do |i|
+  @src[i].each do |dst|
+    src[@group_number[i]] << @group_number[dst] if @group_number[i]!=@group_number[dst]
+  end
+end
+src.map!{ |str| str.uniq}
+
+n.times do |i|
+  src[i].each do |next_num|
+    hash = strs[next_num].clone
+    0.upto(k) do |str_len|
+      str1 = strs[i][str_len]
+      next if str1.nil?
+      strs[next_num].length.times do |j|
+        break if str_len + j > k
+        str2 = strs[next_num][j]
+        hash[str_len+j] ||= str1 + str2
+        hash[str_len+j] = [hash[str_len+j], str1+str2].min
+      end
+    end
+    strs[next_num] = hash
+  end
+end
+
+# 出力
+ans = []
+strs.each do |str|
+  ans << str[k] unless str[k].nil?
+end
+if ans.empty?
+  puts -1
 else
-  puts answers.min
+  puts ans.min
 end
 
