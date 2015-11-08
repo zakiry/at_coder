90d7744ecfed9d4dfc4fd71260787267d3c9b9b0
Ryo Jozaki <zaki0ry@gmail.com>
2015-11-08 20:58:04 +0900

    accept abc008c

--- abc008/c.rb
+++ abc008/c.rb
@@ -1,25 +1,22 @@
 n = gets.chomp.to_i
 
-c = []
+coins = []
 n.times do
-  c << gets.chomp.to_i
+  coins << gets.chomp.to_i
 end
 
-p_list = c.permutation(c.length)
+divisor_count = Array.new(coins.length){0}
 
-sum = 0
-count = 0
-p_list.each do |coins|
-  value = 0
-  coin_sides = Array.new(coins.length){true}
-  coins.length.times do |i|
-    value = value+1 if coin_sides[i]
-    (i+1).upto(coins.length-1) do |j|
-      coin_sides[j] = !coin_sides[j] if coins[j]%coins[i]==0
-    end
+coins.length.times do |i|
+  coins.length.times do |j|
+    divisor_count[j] = divisor_count[j]+1 if coins[j]%coins[i]==0
   end
-  sum = sum+value
-  count = count+1
 end
 
-puts sum.to_f/count
+e = 0.0
+divisor_count.each do |count|
+  e = e + (count/2+count%2).to_f/count
+end
+
+puts e
+
